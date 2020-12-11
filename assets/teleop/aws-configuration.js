24c15a03be80bc4d9bfd38e94ef154b04fee2e4e
lbaitemple <lbai@temple.edu>
2020-12-11 16:10:08 -0500

    Update aws-configuration.js

--- assets/teleop/aws-configuration.js
+++ assets/teleop/aws-configuration.js
@@ -1,11 +1,6 @@
 function getAWSCredentials() {
     var awsConfiguration = {
-        host: "a38zt1esnt42xy-ats.iot.us-east-1.amazonaws.com", // AWS MQTT Endpoint 
-        region: "us-east-1", // AWS Region, ie. us-east-1
-        accessKeyId: "ASIA54JKYWY2RR6VSIY2", // AWS IAM User's Access Key
-        secretAccessKey: "qbvntoblziHDAxCHO3YEU6/W29LVaA+cxhVr1gV6", // AWS IAM User's Secret Key
-        sessionToken: "FwoGZXIvYXdzEA0aDAdHAs9lmQOvxD7OHiK+ATTfEGamjM75kAzwDEf0CKUceWJvQsiI319n65aC3RUJ/1ALgadL5SUUb8uuu7t4bjYpMfP/cay9e3F5HAcwRpEG/02v4s1cuRKIlLJlw+wfzvjp6kMqfb7NnIgSRX7uurH84necGfvuDauVP+cLBLk2CRUqtxOZe+GPLD8c9tEHkD6VPF3HzpSnKXcj6KvaeiTST+/niR/cAjw+XOvdWIGqU+MjwuKJGbozbnuMk3y+cwHMTloPgRZSt06osvUoj5jP/gUyLToSIQ7X8bbztI5PgyU/e157h+yfLC/BpeGLlcIiCUV2dm9nwH2fLmfQNPaVcw=="
-     };
+      };
 
      return awsConfiguration;
 }
