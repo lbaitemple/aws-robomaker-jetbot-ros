934c24d4d50bed07d106fb7584e1afb1a8e8c853
lbaitemple <lbai@temple.edu>
2020-12-31 11:27:22 -0500

    Update and rename compile_arm64.sh to compile_armhf.sh

---
+++ assets/scripts/compile_armhf.sh
@@ -0,0 +1,6 @@
+apt update
+rosdep fix-permissions && rosdep update 
+cd robot_ws
+rosdep install --from-paths src --ignore-src -r -y
+colcon build --build-base armhf_build --install-base armhf_install --cmake-args "-DPYTHON_EXECUTABLE=/usr/bin/python3" "-DPYTHON_INCLUDE_DIR=/usr/include/python3.6m" "-DPYTHON_LIBRARY=/usr/lib/armhf-linux-gnu/libpython3.6m.so"
+colcon bundle --build-base armhf_build --install-base armhf_install --bundle-base arm64_bundle --apt-sources-list /opt/cross/apt-sources.yaml
