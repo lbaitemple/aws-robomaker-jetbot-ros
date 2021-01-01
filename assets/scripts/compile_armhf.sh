e90787d342e677e05d8276b783c920c138b87d62
lbaitemple <lbai@temple.edu>
2020-12-31 20:01:57 -0500

    Update compile_armhf.sh

--- assets/scripts/compile_armhf.sh
+++ assets/scripts/compile_armhf.sh
@@ -3,4 +3,4 @@ rosdep fix-permissions && rosdep update
 cd robot_ws
 rosdep install --from-paths src --ignore-src -r -y
 colcon build --build-base armhf_build --install-base armhf_install --cmake-args "-DPYTHON_EXECUTABLE=/usr/bin/python3" "-DPYTHON_INCLUDE_DIR=/usr/include/python3.6m" "-DPYTHON_LIBRARY=/usr/lib/arm-linux-gnueabihf/libpython3.6m.so/libpython3.6m.so"
-colcon bundle --build-base armhf_build --install-base armhf_install --bundle-base arm64_bundle --apt-sources-list /opt/cross/apt-sources.yaml
+colcon bundle --build-base armhf_build --install-base armhf_install --bundle-base armhf_bundle --apt-sources-list /opt/cross/apt-sources.yaml
