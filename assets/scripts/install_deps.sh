6ccb03692114ea8f7b62d566cd9773ffa781efd4
lbaitemple <lbai@temple.edu>
2021-01-16 19:21:17 -0500

    Update install_deps.sh

--- assets/scripts/install_deps.sh
+++ assets/scripts/install_deps.sh
@@ -1,14 +1,26 @@
 WORK_DIR=$(pwd)
 ROBOT_CERTS_FOLDER=$WORK_DIR/../../robot_ws/src/jetbot_app/config
+ROBOT_SRC_FOLDER=$WORK_DIR/../../robot_ws/src/jetbot_app/src/jetbot_app
+
+TELEOP_CRED=$WORK_DIR/../teleop/aws-configuration.js 
+
 [ ! -d "$ROBOT_CERTS_FOLDER" ] && mkdir -p $ROBOT_CERTS_FOLDER
+
 SIM_CERTS_FOLDER=$WORK_DIR/../../simulation_ws/src/jetbot_sim_app/config
+SIM_SRC_FOLDER=$WORK_DIR/../../simulation_ws/src/jetbot_sim_app/src/jetbot_sim_app
+
 [ ! -d "$SIM_CERTS_FOLDER" ] && mkdir -p $SIM_CERTS_FOLDER
+
 IOTPOLICY="file://../policies/iotpolicy.json"
 IOTPOLICYNAME="JetBotPolicy"
 
 #Configure IoT
 #Create endpoint
+endpoint=`aws iot describe-endpoint --endpoint-type iot:Data-ATS | grep \" | cut -d \" -f4`
+echo ENDPOINT=\"$endpoint\" > $ROBOT_SRC_FOLDER/endpoint.py
+echo ENDPOINT=\"$endpoint\" > $SIM_SRC_FOLDER/endpoint.py
 
+sed -i s"/host: \"\"/host: \"$endpoint\"/" $TELEOP_CRED
 
 #Create IoT Policy
 aws iot create-policy \
