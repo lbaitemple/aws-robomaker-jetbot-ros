bcab8efaf9c70074e86b0ca98314a1b302343fe8
Animesh Bala Ani <animesh.ani@live.com>
2021-08-10 13:09:03 -0400

    Update set_docker.sh

--- assets/scripts/set_docker.sh
+++ assets/scripts/set_docker.sh
@@ -1,25 +1,17 @@
-WORK_DIR=$(pwd)
+# Animesh Ani (ANI717)
+# This Script creates ROS 2 Dashing Docker Image in ARM64 Architecture for Cross Compilation.
 
-# Add ROS dependencies
-cp -a resources/deps/* /etc/ros/rosdep/sources.list.d/
-echo "yaml file:///$WORK_DIR/resources/jetbot.yaml" > /etc/ros/rosdep/sources.list.d/21-customdepenencies.list
+# Remove previously created dockers with same name
+docker ps -a | awk '{ print $1,$2 }' | grep ros2-cross-compile:arm64 | awk '{print $1 }' | xargs -I {} docker stop {}
+docker ps -a | awk '{ print $1,$2 }' | grep ros2-cross-compile:arm64 | awk '{print $1 }' | xargs -I {} docker rm {}
+sudo docker rmi ros2-cross-compile:arm64
 
-# The following command logs in to the an AWS Elastic Container Repository (ECR) to
-# enable your machine to pull a base docker image
-$(aws ecr get-login --no-include-email --registry-ids 593875212637 --region us-east-1)
-apt purge qemu-user qemu-user-static -y
-
-# Install Ubuntu dependencies for cross compilation:
-apt update &&  apt install -y qemu-user qemu-user-static
+# Install Ubuntu dependencies for cross compilation
+apt install qemu-user-static -y
 cp /usr/bin/qemu-aarch64-static .
 
 # Build Docker Container
-docker run --rm --privileged multiarch/qemu-user-static --reset -p yes # This step will execute the registering scripts
-docker build -t ros-cross-compile:arm64 .
+docker build -t ros2-cross-compile:arm64 .
 
-# Fix ros permissions
-rosdep fix-permissions
-sudo -u ubuntu rosdep update
-
-# Remove leftovers
-rm qemu-aarch64-static
+# Remove extra files
+rm ./qemu-aarch64-static
